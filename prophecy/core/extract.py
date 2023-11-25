bab00b3dc42058d1260647b4bc25edb75452ff69
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2023-11-24 16:16:25 -0800

    func to get neuron activations and values for each layer in a given model

--- prophecy/core/extract.py
+++ prophecy/core/extract.py
@@ -1,7 +1,8 @@
 import keras
-import numpy as np
 import pandas as pd
 
+from tensorflow.keras import backend
+from keras.src.engine.keras_tensor import KerasTensor
 
 from prophecy.data.dataset import Dataset
 from prophecy.core.evaluate import get_eval_labels
@@ -21,7 +22,7 @@ def extract_rules(model: keras.Model, dataset: Dataset, split: str):
     """
 
     eval_labels = get_eval_labels(model, dataset, split)
-    #TODO: for test in the previous code the eval_labels are computed with (model.predict(x_val)).argmax(axis=1)
+    # TODO: for test in the previous code the eval_labels are computed with (model.predict(x_val)).argmax(axis=1)
     # that yields different results than the get_eval_labels function
     acc_labels = []
     dec_labels = []
@@ -38,6 +39,42 @@ def extract_rules(model: keras.Model, dataset: Dataset, split: str):
         else:
             mismatch_count = mismatch_count + 1
             dec_labels.append(1000)
-            acc_labels.append(1000) #Misclassified
+            acc_labels.append(1000)  # Misclassified
 
-    print(f"{split.upper()} ACCURACY:", (match_count/(match_count + mismatch_count)) * 100.0)
+    print(f"{split.upper()} ACCURACY:", (match_count / (match_count + mismatch_count)) * 100.0)
+
+
+def get_layer_fingerprint(model_input: KerasTensor, layer: keras.layers.Layer, features: pd.DataFrame):
+    """
+
+    :param model_input: model input
+    :param layer: keras.layers.Layer
+    :param features: pd.DataFrame
+    :return: tuple of (neuron activations, neuron values)
+    """
+    func = backend.function(model_input, [layer.output])
+
+    x_tensor = backend.constant(features)
+    outputs = func(x_tensor)
+
+    return (outputs[0] > 0.0).astype('int'), outputs[0]
+
+
+def get_model_fingerprints(model: keras.Model, dataset: Dataset, split: str):
+    """
+        Collect the neuron values and activations of Train Data and Test Data after each layer
+    :return:
+    """
+
+    # check split is valid
+    if split not in ['train', 'val']:
+        raise ValueError(f"Invalid split {split}")
+
+    fingerprints = {}
+
+    for layer in model.layers:
+        act, val = get_layer_fingerprint(model.input, layer, dataset.splits[split].features)
+        fingerprints[layer.name] = {'act': act, 'val': val}
+        print(f"Fingerprint after {layer.name}. ({act.shape} inputs, {val.shape} neurons)")
+
+    return fingerprints
--- prophecy/main.py
+++ prophecy/main.py
@@ -2,7 +2,7 @@ import argparse
 
 from prophecy.data.dataset import Dataset
 from prophecy.utils.misc import get_model
-from prophecy.core.extract import extract_rules
+from prophecy.core.extract import get_model_fingerprints
 
 
 if __name__ == '__main__':
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     model = get_model(args.model, args.version)
     dataset = Dataset(args.dataset)
 
-    extract_rules(model, dataset, args.split)
+    get_model_fingerprints(model, dataset, args.split)

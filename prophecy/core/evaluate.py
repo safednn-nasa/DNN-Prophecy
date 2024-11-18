8dbaccce7845483c748548ce1b3477eb5965549a
DivyaGOP <divgml@gmail.com>
2024-11-18 12:42:36 -0800

    Update evaluate.py

--- prophecy/core/evaluate.py
+++ prophecy/core/evaluate.py
@@ -19,9 +19,6 @@ def get_eval_labels(model: keras.Model, features: pd.DataFrame, split_name: str)
 
     labels = []
     # confidence = []
-    print(model.input_shape)
-    print(features.shape)
-    features.reshape(values, model.input_shape)
     predictions = model.predict(features, batch_size=128, workers=-1, use_multiprocessing=True, verbose=1)
     # check if the model is binary classification
     if len(predictions[0]) == 1:

fa68399d73aa2b1535b6f5fb001e41114254dee4
DivyaGOP <divgml@gmail.com>
2024-11-29 15:20:47 -0800

    Update helpers.py

--- prophecy/core/helpers.py
+++ prophecy/core/helpers.py
@@ -190,16 +190,16 @@ def calc_prec_recall_f1(suffixes, labels, neurons, signature, cl, VAL, supp=-1) 
     
     if ( 1000 in set(labels)):
         total_labels = np.zeros(TOT_LABELS - 1)
-        print("TOTAL LABELS:",TOT_LABELS)
+      #  print("TOTAL LABELS:",TOT_LABELS)
         for indx in range(0, TOT_LABELS - 1):
             total_labels[indx] = len(np.where(labels == indx)[0])
-            print(indx,":", total_labels[indx])
+       #     print(indx,":", total_labels[indx])
     else:
         total_labels = np.zeros(TOT_LABELS)
-        print("TOTAL LABELS:",TOT_LABELS)
+      #  print("TOTAL LABELS:",TOT_LABELS)
         for indx in range(0, TOT_LABELS):
             total_labels[indx] = len(np.where(labels == indx)[0])
-            print(indx,":", total_labels[indx])
+       #     print(indx,":", total_labels[indx])
         
     
 

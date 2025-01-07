f59293040eb568822382cac6c93e3d4a72295e69
DivyaGOP <divgml@gmail.com>
2025-01-07 12:16:14 -0800

    Add files via upload

---
+++ Marabou/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /content/Marabou
+# Build directory: /content/Marabou/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tools/pybind11-2.10.4")
+subdirs("src")
+subdirs("tools")
+subdirs("regress")

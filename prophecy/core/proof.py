6c302eff2e98d6509eee1c1fc037c5224cb054ec
DivyaGOP <divgml@gmail.com>
2025-01-07 11:50:17 -0800

    Update proof.py

--- prophecy/core/proof.py
+++ prophecy/core/proof.py
@@ -1,1 +1,9 @@
+import os
+
+path = os.environ['PATH']
+print(path)
+print(os.environ['PATH'])
+os.environ['PATH'] = path + ':/ProphecyPlus/Marabou/Marabou_bld:/ProphecyPlus/Marabou/Marabou_bld/build:/ProphecyPlus/Marabou/Marabou_bld/build/bin'
+print(os.environ['PATH'])
+
 

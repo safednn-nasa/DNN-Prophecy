63c37036c1c3f3215b1c724a58bc3206e3b02088
DivyaGOP <divgml@gmail.com>
2025-01-08 15:04:12 -0800

    Update main.py

--- prophecy/main.py
+++ prophecy/main.py
@@ -100,7 +100,7 @@ def run_prove_command(lab: int):
     print("FEATURES:", np.shape(train_features))
     print("LABELS:", np.shape(train_labels))
     
-    prove_marabou = RulesProve(model=model, onnx_model_nm=onnx_model, layer_nm = top_rule_layer, neurons=rule_neurons_list, sig=rule_sig_list,features=train_features, labels=train_labels)
+    #prove_marabou = RulesProve(model=model, onnx_model_nm=onnx_model, layer_nm = top_rule_layer, neurons=rule_neurons_list, sig=rule_sig_list,features=train_features, labels=train_labels)
     #results = prove_marabou()
 
 

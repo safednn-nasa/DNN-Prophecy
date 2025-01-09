927e483eec5dbe5901571fc8e809e94961742e3d
DivyaGOP <divgml@gmail.com>
2025-01-08 16:36:07 -0800

    Update proof1.py

--- prophecy/core/proof1.py
+++ prophecy/core/proof1.py
@@ -98,7 +98,7 @@ class RulesProve:
         print("INPUT EXAM:", inp_ex[0])
         print("FINGERPRINT EXAM:", finger_ex[0])
 
-        return (x_train_min, x_train_max, x_train_min_layer, x_train_max_layer, fngprnt_min_layer, fngprnt_max_layer, inp_ex[0], fngr_ex[0])
+        return (x_train_min, x_train_max, x_train_min3, x_train_max3, fngprnt_min3, fngprnt_max3, inp_ex[0], finger_ex[0])
         
     def __call__(self, **kwargs) -> str:
         results = ""

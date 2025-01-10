843be5af4cbdb8ef0f2548589d7c78dc80823d2f
DivyaGOP <divgml@gmail.com>
2025-01-09 19:31:20 -0800

    Update solveMarabou.py

--- Marabou/solveMarabou.py
+++ Marabou/solveMarabou.py
@@ -1,8 +1,8 @@
 import os
 import numpy as np
-#from prophecy.core.Marabou.maraboupy import Marabou
-#from maraboupy.MarabouCore import *
-#from maraboupy.MarabouPythonic import *
+from Marabou.maraboupy import Marabou
+from Marabou.maraboupy.MarabouCore import *
+from Marabou.maraboupy.MarabouPythonic import *
 
 class SolveMarabou:
   def __init__(self, onnx_model_nm: str, onnx_layer_nm: str, x_train_min_layer: np.array, x_train_max_layer: np.array, fngprnt_min_layer: np.array, fngprnt_max_layer: np.array, lab: int):

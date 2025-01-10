ba0d5d2eb39bf04f5788a553b478e6365e2a2122
DivyaGOP <divgml@gmail.com>
2025-01-10 13:55:11 -0800

    Update proof1.py

--- prophecy/core/proof1.py
+++ prophecy/core/proof1.py
@@ -116,9 +116,9 @@ class RulesProve:
         os.system("pwd")
         os.system("ls -lt")
         #from maraboupy import Marabou
-        options = Marabou.createOptions(verbosity = 1, numWorkers=1, numBlasThreads=1,snc=True)
+        options = maraboupy.Marabou.createOptions(verbosity = 1, numWorkers=1, numBlasThreads=1,snc=True)
         filename = "./resources/onnx/cnn_max_mninst2.onnx"
-        network_a = Marabou.read_onnx(filename)
+        network_a =  maraboupy.Marabou.read_onnx(filename)
         
         #os.system("python /content/ProphecyPlus/solveMarabou/main.py")
 

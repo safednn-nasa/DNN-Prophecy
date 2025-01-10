f929750f54e59a29c9b10e3600259051fcc6064c
DivyaGOP <divgml@gmail.com>
2025-01-09 18:08:48 -0800

    Add files via upload

---
+++ prophecy/core/Marabou/build/CMakeFiles/MarabouHelperTest.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for MarabouHelperTest.
+# This may be replaced when dependencies are built.
---
+++ prophecy/core/Marabou/build/CMakeFiles/build_input_parsers.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/build_input_parsers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

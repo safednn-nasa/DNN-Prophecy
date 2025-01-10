38671d17ba824e51e2d62461dd7ccc02161b7d3d
DivyaGOP <divgml@gmail.com>
2025-01-09 18:09:41 -0800

    Add files via upload

---
+++ prophecy/core/Marabou/build/regress/regress4/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /content/Marabou/regress/regress4
+# Build directory: /content/Marabou/build/regress/regress4
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

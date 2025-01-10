0540df34b365c2144c86fd586260f953feb8fdae
DivyaGOP <divgml@gmail.com>
2025-01-10 13:22:32 -0800

    Update main.py

--- solveMarabou/main.py
+++ solveMarabou/main.py
@@ -8,5 +8,4 @@ if __name__ == '__main__':
   options = Marabou.createOptions(verbosity = 1, numWorkers=1, numBlasThreads=1,snc=True)
   filename = "./resources/onnx/cnn_max_mninst2.onnx"
   network_a = Marabou.read_onnx(filename)
-
-exit()
+  exit()

376440504043fca2a71781782cd7f602cd07d0e5
DivyaGOP <divgml@gmail.com>
2025-01-10 16:34:03 -0800

    Update proof1.py

--- prophecy/core/proof1.py
+++ prophecy/core/proof1.py
@@ -109,84 +109,67 @@ class RulesProve:
         (x_train_min, x_train_max, x_train_min_layer, x_train_max_layer, fngprnt_min_layer, fngprnt_max_layer, inp_ex, fngr_ex) = self.get_bounds()
 
         
-        #path = os.environ['PATH']
-        #print(path)
-        #os.environ['PATH'] = path + ':/content/drive/MyDrive/Marabou_bld:/content/drive/MyDrive/Marabou_bld/build:/content/drive/MyDrive/Marabou_bld/build/bin'
-        #print(os.environ['PATH'])
+        onnx_model_nm=self.onnx_path
+        onnx_layer_nm="dense_14_1/Identity:0"
+        lab=self.lab
+        #x_train_min_layer=x_train_min_layer,x_train_max_layer=x_train_max_layer,fngprnt_min_layer=fngprnt_min_layer,fngprnt_max_layer=fngprnt_max_layer,
+        
+        options1 = Marabou.createOptions(verbosity = 1, numWorkers=1, numBlasThreads=1,snc=True)
+        filename = onnx_model_nm
+        network_a = Marabou.read_onnx(filename)
 
-        #os.chdir('/content/drive/MyDrive/Marabou_bld')
-        #os.system("pwd")
-        #os.system("ls -lt")
-       
-        options = Marabou.createOptions(verbosity = 1, numWorkers=1, numBlasThreads=1,snc=True)
-        filename = self.onnx_path
-        network_a =  Marabou.read_onnx(filename)
-        
-        #os.system("python /content/ProphecyPlus/solveMarabou/main.py")
-
-        
-        
-        #onnx_model_nm=self.onnx_path
-        #onnx_layer_nm="dense_14_1/Identity:0"
-        #lab=self.lab
-        ##x_train_min_layer=x_train_min_layer,x_train_max_layer=x_train_max_layer,fngprnt_min_layer=fngprnt_min_layer,fngprnt_max_layer=fngprnt_max_layer,
-        
-        #options1 = Marabou.createOptions(verbosity = 1, numWorkers=1, numBlasThreads=1,snc=True)
-        #filename = onnx_model_nm
-        #network_a = Marabou.read_onnx(filename)
-
-        #print("INPUT VARS")
-        #invars = self.network_a.inputVars[0][0].flatten()
-        #print(invars)
+        print("INPUT VARS")
+        invars = self.network_a.inputVars[0][0].flatten()
+        print(invars)
     
-        #for indx in range(0,len(invars)):
-        #    i = invars[indx]
-        #    v = Var(i)
-        #    network_a.setLowerBound(i,x_train_min_layer[i])
-        #    network_a.setUpperBound(i,x_train_max_layer[i])
-        #    #network_a.setLowerBound(i,inp_ex[0][indx])
-        #    #network_a.setUpperBound(i,inp_ex[0][indx])
+        for indx in range(0,len(invars)):
+            i = invars[indx]
+            v = Var(i)
+            network_a.setLowerBound(i,x_train_min_layer[i])
+            network_a.setUpperBound(i,x_train_max_layer[i])
+            #network_a.setLowerBound(i,inp_ex[0][indx])
+            #network_a.setUpperBound(i,inp_ex[0][indx])
 
-        #print("LAYER VARS")
-        #neurons = network_a.layerNameToVariables[onnx_layer_nm][0]
-        #print(np.shape(neurons))
+        print("LAYER VARS")
+        neurons = network_a.layerNameToVariables[onnx_layer_nm][0]
+        print(np.shape(neurons))
     
-        #for indx in range(0, len(neurons)):
-        #    neuron_indx = neurons[indx] - neurons[0]
-        #    network_a.setLowerBound(neurons[indx], fngprnt_min_layer[neuron_indx])
-        #    network_a.setUpperBound(neurons[indx], fngprnt_max_layer[neuron_indx])
-        #    #network_a.setLowerBound(dense_14_neurons[indx], finger_ex[0][neuron_indx] - 0.1)
-        #    #network_a.setUpperBound(dense_14_neurons[indx], finger_ex[0][neuron_indx] + 0.1)
+        for indx in range(0, len(neurons)):
+            neuron_indx = neurons[indx] - neurons[0]
+            network_a.setLowerBound(neurons[indx], fngprnt_min_layer[neuron_indx])
+            network_a.setUpperBound(neurons[indx], fngprnt_max_layer[neuron_indx])
+            #network_a.setLowerBound(dense_14_neurons[indx], finger_ex[0][neuron_indx] - 0.1)
+            #network_a.setUpperBound(dense_14_neurons[indx], finger_ex[0][neuron_indx] + 0.1)
 
-        #print("OUTPUT VARS")
-        #outvars = network_a.outputVars[0].flatten()
-        #print(outvars)
+        print("OUTPUT VARS")
+        outvars = network_a.outputVars[0].flatten()
+        print(outvars)
 
-        #rule_label = lab
-        #prove = True
-        #for label in range(0,  len(outvars)):
-        #    if (label == rule_label):
-        #        continue
-        #    label_var = Var(outvars[label])
-        #    for indx in range(0,  len(outvars)):
-        #        v = Var(outvars[indx])
-        #        if (indx == label):
-        #            continue
-        #        network_a.addConstraint(label_var >= v + 0.001)
-        #        print(v, ":",indx)
+        rule_label = lab
+        prove = True
+        for label in range(0,  len(outvars)):
+            if (label == rule_label):
+                continue
+            label_var = Var(outvars[label])
+            for indx in range(0,  len(outvars)):
+                v = Var(outvars[indx])
+                if (indx == label):
+                    continue
+                network_a.addConstraint(label_var >= v + 0.001)
+                print(v, ":",indx)
         
-         #   sat_unsat,vals,stats = network_a.solve(options = options1)
-         #   print("sat_unsat:", sat_unsat)
+            sat_unsat,vals,stats = network_a.solve(options = options1)
+            print("sat_unsat:", sat_unsat)
             
-         #   if (sat_unsat == 'sat'):
-         #       print("SAT for label:", label)
-         #       print("vals:", vals)
-         #       prove = False
-         #       break
-         #   else:
-         #       print("UNSAT for label:", label)
+            if (sat_unsat == 'sat'):
+                print("SAT for label:", label)
+                print("vals:", vals)
+                prove = False
+                break
+            else:
+                print("UNSAT for label:", label)
 
-        #if (prove == True):
-        #    print("Rule Proved!!")
+        if (prove == True):
+            print("Rule Proved!!")
         
         return results

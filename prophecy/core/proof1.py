da269db0a0cc0ff674bbb220a1af501836f03f3d
DivyaGOP <divgml@gmail.com>
2025-01-13 12:02:26 -0800

    Update proof1.py

--- prophecy/core/proof1.py
+++ prophecy/core/proof1.py
@@ -105,8 +105,8 @@ class RulesProve:
 
         return (x_train_min, x_train_max, x_train_min3, x_train_max3, fngprnt_min3, fngprnt_max3, inp_ex[0], finger_ex[0])
         
-    def __call__(self, **kwargs) -> str:
-        results = ""
+    def __call__(self, **kwargs) -> bool:
+        results = False
 
         (x_train_min, x_train_max, x_train_min_layer, x_train_max_layer, fngprnt_min_layer, fngprnt_max_layer, inp_ex, finger_ex) = self.get_bounds()
 
@@ -191,5 +191,6 @@ class RulesProve:
 
         if (prove == True):
             print("Rule Proved!!")
+            results = True
         
         return results

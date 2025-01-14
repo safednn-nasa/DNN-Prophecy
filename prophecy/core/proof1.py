9e1c402071670fb2624887bcbcdde2f10e184fb5
DivyaGOP <divgml@gmail.com>
2025-01-14 09:53:00 -0800

    Update proof1.py

--- prophecy/core/proof1.py
+++ prophecy/core/proof1.py
@@ -13,6 +13,9 @@ from typing import Tuple, Union
 from tqdm import tqdm
 from pathlib import Path
 
+import time
+import func_timeout
+
 from prophecy.core.helpers import check_pattern, get_suffix_cluster
 sys.path.append('/content/drive/MyDrive/Marabou')
 from maraboupy import Marabou

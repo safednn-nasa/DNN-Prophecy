dacfa08aed7c7b021e83b9d88fa5f737a97e4e78
DivyaGOP <divgml@gmail.com>
2025-01-14 09:47:56 -0800

    Update proof1.py

--- prophecy/core/proof1.py
+++ prophecy/core/proof1.py
@@ -110,9 +110,6 @@ class RulesProve:
         (x_train_min, x_train_max, x_train_min_layer, x_train_max_layer, fngprnt_min_layer, fngprnt_max_layer, inp_ex, finger_ex) = self.get_bounds()
 
         results = False
-        iter = 0
-        while (results == False):
-            
         
         onnx_model_nm=self.onnx_path
         h5_onnx_map = np.genfromtxt(self.onnx_map, delimiter=',', dtype=str)

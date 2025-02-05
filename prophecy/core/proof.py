10a67b7a97317087fb106e65efa269de17dad425
DivyaGOP <divgml@gmail.com>
2025-02-05 12:42:59 -0800

    Update proof.py

--- prophecy/core/proof.py
+++ prophecy/core/proof.py
@@ -170,7 +170,7 @@ class RulesProve:
         vals = None
         stats = None
 
-        options1 = Marabou.createOptions(verbosity = 1,timeoutInSeconds=1000)
+        options1 = Marabou.createOptions(verbosity = 1,timeoutInSeconds=1800)
         sat_unsat,vals,stats = network_a.solve(options = options1)
            
         print("sat_unsat:", sat_unsat)
